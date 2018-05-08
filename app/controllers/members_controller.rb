46b7a89595aeb1026dd60e73874e3f39e13e4a53
EC2 Default User <ec2-user@ip-172-31-31-252.us-east-2.compute.internal>
2018-05-08 04:57:37 +0000

    first commit

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,47 @@
+class MembersController < ApplicationController
+  def new
+    @member = Member.new
+  end
+  
+  
+  def create
+    @member = Member.new(member_params)
+    
+    @member.save
+    redirect_to members_show_url(id: @member.id)
+  end
+  
+
+  def show
+    @member = Member.find(params[:id])
+  end
+  
+  def edit
+    @member = Member.find(params[:id])
+    
+  end
+  
+  def update
+    @member = Member.find(params[:id])
+    @member.update_attributes(member_params)
+    redirect_to members_show_url(id: @member.id)
+  end
+
+  def index
+    @members = Member.all.sort
+    #viewで使うなら｢@変数名｣でないといけない
+  end
+  
+  def delete
+    n = Member.find(params[:id])
+    n.destroy
+    redirect_to members_index_path
+  end
+  
+  
+private
+  def member_params
+    params.require(:member).permit(:name, :age)
+  end
+  
+end
